6ef38c1abbe3db06f86b29ea227ace05f03c978e
Maira Bello <maira.araujo@liferay.com>
2015-09-28 10:43:38 -0300

    Includes gulp-metal

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,12 @@
 'use strict';
 
-var del = require('del');
-var gulp = require('gulp');
-var plugins = require('gulp-load-plugins')();
-var metaljs = require('metaljs');
-var runSequence = require('run-sequence');
+var metal = require('gulp-metal');
 
-metaljs({
-  buildDest: 'metaljs/build',
-  buildSrc: 'metaljs/src/**/*.js',
-  bundleFileName: 'list.js',
-  soyDest: 'metaljs/src',
-  soySrc: 'metaljs/src/**/*.soy'
-});
-
-gulp.task('build', function(done) {
-  runSequence('clean', 'build:globals', done);
-});
-
-gulp.task('clean', function(done) {
-  del(['build'], done);
-});
-
-gulp.task('lint', function() {
-  return gulp.src('./**/*.js')
-    .pipe(plugins.jshint())
-    .pipe(plugins.jshint.reporter(require('jshint-stylish')));
+metal.registerTasks({
+	buildDest: 'build/metaljs',
+	buildSrc: 'src/metaljs/**/*.js',
+	bundleFileName: 'list.js',
+	moduleName: 'metal-perf-list',
+	soyDest: 'src/metaljs',
+	soySrc: 'src/metaljs/**/*.soy'
 });
