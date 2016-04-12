aaf46332650851de40fe2419260ba348e429b364
Maira Bello <maira.araujo@liferay.com>
2016-04-12 16:37:12 -0300

    Displays graph after tests are done

--- tests/MetalPerf.js
+++ tests/MetalPerf.js
@@ -1,6 +1,8 @@
 'use strict';
 
-this.MetalPerf = { // jshint ignore:line
+window.MetalPerf = { // jshint ignore:line
+	results: {},
+
 	run(suite) {
 		suite
 			.on('start', function(event) {
@@ -9,11 +11,48 @@ this.MetalPerf = { // jshint ignore:line
 			.on('cycle', function(event) {
 				var text = String(event.target).substr(event.target.name.length + 3);
 				document.querySelector('#' + event.target.name).textContent = text;
+				MetalPerf.results[event.target.name] = event.target.hz;
 			})
 			.on('complete', function() {
 				var result = document.querySelector('#result');
 				result.textContent = 'Fastest: ' + this.filter('fastest').map('name');
 				metal.dom.removeClasses(result, 'hidden');
+
+        var chartElement = document.querySelector('#chart');
+				metal.dom.removeClasses(chartElement, 'hidden');
+				var chart = new window.Chart(chartElement, {
+          type: 'bar',
+          data: {
+  			    labels: ['ops/sec'],
+  			    datasets: [
+  			        {
+                    backgroundColor: 'rgba(0,0,256,0.2)',
+  			            label: 'Metal',
+  			            data: [MetalPerf.results.Metal]
+  			        },
+  			        {
+                    backgroundColor: 'rgba(0,256,0,0.2)',
+  			            label: 'React',
+  			            data: [MetalPerf.results.React]
+  			        },
+  			        {
+                    backgroundColor: 'rgba(256,0,0,0.2)',
+  			            label: 'YUI',
+  			            data: [MetalPerf.results.YUI]
+  			        }
+  			    ]
+          },
+          options: {
+            responsive: false,
+            scales: {
+      				yAxes: [{
+      						ticks: {
+      							beginAtZero: true
+      						}
+      					}]
+      			}
+          }
+				});
 			})
 			.run({async: true});
 	}
